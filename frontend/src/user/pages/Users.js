ea2df288c093b1951d2122d5dd8bacc79762c7dc
alexeispirit <alexeispirit@gmail.com>
2019-12-06 14:38:02 +0500

    39-adding-UsersList-component

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Dux",
+      image:
+        "https://images.pexels.com/photos/532220/pexels-photo-532220.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
