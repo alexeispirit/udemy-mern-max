a4e26b8fa1bbf1d6f42ea96d390ac7a8a1ef34f2
alexeispirit <alexeispirit@gmail.com>
2019-12-10 12:08:04 +0500

    132-adding-user-login

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -59,13 +59,20 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    const error = new HttpError("Logging up failed", 500);
+    return next(error);
+  }
 
-  if (!identifiedUser || identifiedUser.password !== password) {
-    return next(new HttpError("Could not identified user", 401));
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError("Invalid credentials", 401);
+    return next(error);
   }
 
   res.status(200).json({ message: "logged in" });
