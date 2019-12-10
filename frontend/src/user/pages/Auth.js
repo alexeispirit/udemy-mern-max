f880747916cb78e95a715dd058827f44e0a89f2a
alexeispirit <alexeispirit@gmail.com>
2019-12-10 15:25:20 +0500

    148-sending-login-request

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -56,9 +56,31 @@ const Auth = () => {
   const authSubmitHandler = async event => {
     event.preventDefault();
 
+    setIsLoading(true);
     if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || "Something went wrong");
+      }
     } else {
-      setIsLoading(true);
       try {
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
@@ -76,7 +98,6 @@ const Auth = () => {
         if (!response.ok) {
           throw new Error(responseData.message);
         }
-        console.log(responseData);
         setIsLoading(false);
         auth.login();
       } catch (err) {
