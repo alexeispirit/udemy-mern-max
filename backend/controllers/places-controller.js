971cfc28ca5b6090f2109ad794539ffc5f76b0e9
alexeispirit <alexeispirit@gmail.com>
2019-12-10 09:22:46 +0500

    93-adding-controllers

---
+++ backend/controllers/places-controller.js
@@ -0,0 +1,55 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    imageUrl:
+      "https://media.ceetiz.com/activity/ESBNYC003/thumbnails/642x450/shutterstock_129799142.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: "u1"
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    imageUrl:
+      "https://media.ceetiz.com/activity/ESBNYC003/thumbnails/642x450/shutterstock_129799142.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: "u2"
+  }
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => p.id === placeId);
+
+  if (!place) {
+    return next(new HttpError("Could not find place for provided id", 404));
+  }
+
+  res.json({ place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find(p => p.creator === userId);
+
+  if (!place) {
+    throw new HttpError("Could not find place for provided user id", 404);
+  }
+
+  res.json({ place });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,58 +1,11 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const placesController = require("../controllers/places-controller");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world",
-    imageUrl:
-      "https://media.ceetiz.com/activity/ESBNYC003/thumbnails/642x450/shutterstock_129799142.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584
-    },
-    creator: "u1"
-  },
-  {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world",
-    imageUrl:
-      "https://media.ceetiz.com/activity/ESBNYC003/thumbnails/642x450/shutterstock_129799142.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584
-    },
-    creator: "u2"
-  }
-];
+router.get("/:pid", placesController.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find(p => p.id === placeId);
-
-  if (!place) {
-    return next(new HttpError("Could not find place for provided id", 404));
-  }
-
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find(p => p.creator === userId);
-
-  if (!place) {
-    throw new HttpError("Could not find place for provided user id", 404);
-  }
-
-  res.json({ place });
-});
+router.get("/user/:uid", placesController.getPlaceByUserId);
 
 module.exports = router;
