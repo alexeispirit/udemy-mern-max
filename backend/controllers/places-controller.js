8b1b9d1ba0b77ef193230efa331ce4675ce8bf27
alexeispirit <alexeispirit@gmail.com>
2019-12-10 10:38:05 +0500

    101-validating-patch-requests-and-user-routes

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -79,6 +79,13 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError("Invalid inputs passed, please check your data", 422)
+    );
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
@@ -94,6 +101,11 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+
+  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+    return next(new HttpError("Could not find place with that id", 404));
+  }
+
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
 
   res.status(200).json({ message: "Deleted place" });
