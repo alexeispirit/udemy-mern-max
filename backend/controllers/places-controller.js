838fc707e450dbb0d4503d9b2fc0238a1cf8fc22
alexeispirit <alexeispirit@gmail.com>
2019-12-10 11:32:15 +0500

    124-creating-and-storing-documents-in-database

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -3,6 +3,7 @@ const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 const getCoordsForAddress = require("../util/location");
+const Place = require("../models/place");
 
 let DUMMY_PLACES = [
   {
@@ -72,16 +73,22 @@ const createPlace = async (req, res, next) => {
     return next(err);
   }
 
-  const createdPlace = {
-    id: uuid(),
+  const createdPlace = new Place({
     title,
     description,
-    location: coordinates,
     address,
+    location: coordinates,
+    image:
+      "https://media.ceetiz.com/activity/ESBNYC003/thumbnails/642x450/shutterstock_129799142.jpg",
     creator
-  };
+  });
 
-  DUMMY_PLACES.push(createdPlace);
+  try {
+    await createdPlace.save();
+  } catch (err) {
+    const error = new HttpError("Creating place failed", 500);
+    return next(error);
+  }
 
   res.status(201).json({ place: createdPlace });
 };
