f4c9b669fb034f0b6260b6221326fddf092dee22
alexeispirit <alexeispirit@gmail.com>
2019-12-10 12:37:57 +0500

    137-getting-places-alternative

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -28,23 +28,27 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
+  // let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    // places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate("places");
   } catch (err) {
     const error = new HttpError("Could not find place", 500);
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError("Could not find place for provided user id", 404)
     );
   }
 
-  res
-    .status(200)
-    .json({ places: places.map(place => place.toObject({ getters: true })) });
+  res.status(200).json({
+    places: userWithPlaces.places.map(place =>
+      place.toObject({ getters: true })
+    )
+  });
 };
 
 const createPlace = async (req, res, next) => {
